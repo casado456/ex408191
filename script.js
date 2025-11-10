3bdae2563db317615761540b24533c456616830f
mohammad <arasto456@gmail.com>
2025-11-10 22:09:12 +0330

    firstproject

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+let coin =0
+let trap = 0
+let cointrap = 0
 
-
-for (let i = 1; i <= 20; i++) {
+for (let i = 1; i <= 50; i++ ) {
     if   (i % 3 === 0 && i % 5 === 0){
         console.log(i, "cointrap");
+        cointrap++
+        
     }if (i % 5 === 0) {
         console.log(i,"trap");
-    }if (i % 3 === 0)
+        trap++
+        
+    }if (i % 3 === 0){
         console.log(i, "coin");
+        coin++
+        
+    }
+    
     
 }
+console.log("coin" + " "+ coin);
+console.log("trap" + " " + trap);
+console.log("cointrap" + " " + cointrap);
+
+
